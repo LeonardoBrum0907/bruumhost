8948035b8c0628fc8573e8e3326d3c3e8a6c0a50
Leonardo Brum <leonardobrum.dev@gmail.com>
2025-12-24 12:43:01 -0300

    lint: Removed unused variables

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import LightPillar from './components/LightPillar.tsx'
 
 createRoot(document.getElementById('root')!).render(
    <StrictMode>
