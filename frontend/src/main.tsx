ff28b937c507280b9f4d7bd0e33dc3820b6db7f6
Leonardo Brum <leonardobrum.dev@gmail.com>
2025-12-24 09:13:00 -0300

    refactor: Remove LightPillar component from main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,19 +7,5 @@ import LightPillar from './components/LightPillar.tsx'
 createRoot(document.getElementById('root')!).render(
    <StrictMode>
       <App />
-
-      <LightPillar
-         topColor="#5227FF"
-         bottomColor="#FF9FFC"
-         intensity={1.0}
-         rotationSpeed={0.5}
-         glowAmount={0.002}
-         pillarWidth={3.0}
-         pillarHeight={0.4}
-         noiseIntensity={0.5}
-         pillarRotation={25}
-         interactive={false}
-         mixBlendMode="normal"
-      />
    </StrictMode>,
 )
