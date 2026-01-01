ad21e0dec57b04294113f7b50aaa45046c04c044
Leonardo Brum <leonardobrum.dev@gmail.com>
2026-01-01 16:59:28 -0300

    chore: Update Vite configuration to include React plugin and Vitest setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,20 @@
+/// <reference types="vitest/config" />
+
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
    plugins: [
       tailwindcss(),
+      react(),
    ],
+   test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/tests/setup.ts',
+   },
    resolve: {
       alias: {
          '@': path.resolve(__dirname, './src'),
