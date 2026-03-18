ab28314048c4c10a22f47639d8c0e334db26f338
Leonardo Brum <leonardobrum.dev@gmail.com>
2026-03-17 21:49:00 -0300

    feat: Add core ports interfaces for Clock, ImageChecker, DockerClient, and Redis

---
+++ api-server/src/core/ports.ts
@@ -0,0 +1,38 @@
+type UnixMs = number
+
+export interface ClockPort {
+   now(): UnixMs
+}
+
+export interface ImageCheckerPort {
+   ensureImageExists(imageName: string): Promise<void>
+}
+
+interface DockerCreateContainerOptions {
+   Image: string
+   name: string
+   Env: string[]
+   HostConfig: {
+     AutoRemove: boolean
+     NetworkMode: string
+   }
+   AttachStdout: boolean
+   AttachStderr: boolean
+}
+
+interface DockerContainerPort {
+   start(): Promise<void>
+}
+
+export interface DockerClientPort {
+   createContainer(options: DockerCreateContainerOptions): Promise<DockerContainerPort>
+}
+
+export interface RedisPort {
+   set(
+      key: string,
+      value: string,
+      mode: 'EX',
+      ttlSeconds: number
+   ): Promise<string>
+}
